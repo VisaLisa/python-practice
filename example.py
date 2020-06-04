84600ee15856ded1d9c410be6ec0ea7aeec517e6
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-06-04 11:31:17 -0500

    updated shirt.py and added shirt one attributes in example.py

--- example.py
+++ example.py
@@ -4,7 +4,12 @@ shirt_one = Shirt('red', 'M', 'long_sleeve', 45)
 shirt_two = Shirt('orange', 'S', 'short_sleeve', 30)
 
 print(shirt_one.price)
+print(shirt_two.price)
 print(shirt_two.color)
 
 shirt_two.change_price(45)
 print(shirt_two.price)
+
+shirt_one.color = 'yellow'
+shirt_one.size = 'L'
+shirt_one.price = 39
